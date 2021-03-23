cf2a6eb31abdf78d46733a0e22d3123b084b6a50
Sumito Izumita <sumito@izumita.com>
2021-03-23 14:31:00 +0900

    fun

--- main.ml
+++ main.ml
@@ -1,147 +1,75 @@
+(* 
+
+(fun x y -> x (fun z -> y z)) f g a
+
+ *)
+
+let trim str =
+  if str = "" then "" else
+  let search_pos init p next =
+    let rec search i =
+      if p i then raise(Failure "empty") else
+      match str.[i] with
+      | ' ' | '\n' | '\r' | '\t' -> search (next i)
+      | _ -> i
+    in
+    search init
+  in
+  let len = String.length str in
+  try
+    let left = search_pos 0 (fun i -> i >= len) (succ)
+    and right = search_pos (len - 1) (fun i -> i < 0) (pred)
+    in
+    String.sub str left (right - left + 1)
+  with
+  | Failure "empty" -> ""
+
+
 type token =
-  | Cons of int
-  | Plus
-  | Minus
-  | Div
-  | Mul
-  | Lb
+    Lb
   | Rb
-  | Bracket of token list
-  | Err
+  | Const of string
+  | Fun
+  | Equal
 
 type expr =
-  | Func of string * expr list * expr
-  | Call of string * expr list
-  | Const of int
-  | EOF
-  | Unit
-  | Test of token
+    Var of string
+  | Assign of string list * expr
+  | Call of expr * expr
 
+let expect_right_arrow t =
+  let l = String.length t in
+  let rec loop i =
+    if t.[i] = '-' && t.[i+1] = '>' then (String.sub t 0 i, String.sub t (i+2) (l-i-2)) else loop @@ i+1
+  in loop 0
 
-let numbers = [
-  '0';
-  '1';
-  '2';
-  '3';
-  '4';
-  '5';
-  '6';
-  '7';
-  '8';
-  '9';
-]
+let l = String.length
 
-let print_tokens lst =
-  List.map (fun t ->
-    match t with
-      | Cons i -> "Const:" ^ (string_of_int i)
-      | Plus -> "+"
-      | Minus -> "-"
-      | Div -> "/"
-      | Mul -> "*"
-      | Lb -> "("
-      | Rb -> ")"
-      | _ -> "..."
-    ) lst
-  |> String.concat " "
-  |> print_endline
-
-let expect_int p base =
-  let buff = Buffer.create 16 in
-  let len = String.length base in
-  let rec loop pointer =
-    if pointer >= len then pointer else
-    let c = String.get base pointer in
-    if List.mem c numbers
-      then 
-        begin
-          Buffer.add_char buff c;
-          loop @@ pointer + 1
-        end
-      else pointer       
-  in 
-    let pointer = loop p in (pointer, int_of_string @@ Buffer.contents buff)
-
-
-
-let tokenize base =
-  let rec loop pointer result =
-    if pointer >= (String.length base) then List.rev result else
-    match String.get base pointer with
-      | ' ' -> loop (pointer+1) result
-      | '\n' -> loop (pointer+1) result
-      | '\t' -> loop (pointer+1) result
-      | '+' -> loop (pointer+1) @@ Plus :: result
-      | '-' -> loop (pointer+1) @@ Minus :: result
-      | '/' -> loop (pointer+1) @@ Div :: result
-      | '*' -> loop (pointer+1) @@ Mul :: result
-      | '(' -> loop (pointer+1) @@ Lb :: result
-      | ')' -> loop (pointer+1) @@ Rb :: result
+let parse text =
+  let text_length = String.length text in
+  let take_1 t = if l t >= 1 then (String.sub t 0 1, String.sub t 1 (l t - 1)) else  ("", t) in
+  let take_2 t = if l t >= 2 then (String.sub t 0 2, String.sub t 2 (l t -2)) else ("", t) in
+  let take_3 t = if l t >= 3 then (String.sub t 0 3, String.sub t 3 (l t -3)) else ("", t) in
+  let take_4 t = if l t >= 4 then (String.sub t 0 4, String.sub t 4 (l t -4)) else ("", t) in
+  let rec take_expr t =
+    match take_4 t with
+      | ("fun ", rest) -> take_fun rest
       | _ ->
-    let (pointer, i) = expect_int pointer base
-    in loop pointer @@ (Cons i) :: result
-  in loop 0 []
-
-
-let expect_any tokens target =
-  let rec loop lst result =
-    match lst with
-    | [] -> ([], tokens)
-    | first :: rest ->
-      if first = target
-        then (List.rev result, rest)
-        else loop rest @@ first :: result
-  in loop tokens []
-
-let parse tokens =
-  let rec loop lst result =
-    match lst with
-      | (Cons x) :: [] -> Const x
-      | (Cons x) :: [Rb] -> Const x
-      | (Bracket l) :: [] -> loop l []
-      | Lb :: rest ->
-        (match bracket rest true with
-          | Bracket [] -> Unit
-          | Bracket l -> loop l []
-          | _ -> EOF)
+    match take_3 t with
       | _ ->
-        if List.mem Lb lst 
-          then match bracket lst false with
-            | Bracket [] -> Unit
-            | Bracket l -> loop l []
-            | _ -> split_symbols lst
-          else split_symbols lst
+    match take_2 t with
+      | _ ->
+    match take_1 t with
+      | (" ", rest) -> take_expr rest
+      | (a, rest) -> Var a
+  and take_fun t =
+    let (arg_list, exp) = take_fun_args t in
+    Assign (arg_list, take_expr exp)
+  and take_fun_args t =
+    let (args, rest) = expect_right_arrow t in
+    let arg_list = args |> String.split_on_char ' ' |> List.filter (fun a -> a <> "") in
+    (arg_list, rest)
 
-  and split_symbols lst =
-    if List.mem Plus lst then
-        let (r, l) = expect_any lst Plus in Call ("+", [loop r []; loop l []])
-      else if List.mem Minus lst then
-        let (r, l) = expect_any lst Minus in Call ("-", [loop r []; loop l []])
-      else if List.mem Div lst then
-        let (r, l) = expect_any lst Div in Call ("/", [loop r []; loop l []])
-      else if List.mem Mul lst then
-        let (r, l) = expect_any lst Mul in Call ("*", [loop r []; loop l []])
-      else EOF
+  in take_expr text
 
-  and bracket tokens_ is_expcted =
-    let rec loop_ lst result =
-      match lst with
-      | [] -> if is_expcted then (Err, []) else (Bracket (List.rev result), [])
-      | first :: rest ->
-        if first = Lb
-          then
-            let (bracket, rest) = loop_ rest [] 
-            in loop_ rest @@ bracket :: result
-        else if first = Rb
-          then (Bracket (List.rev result), rest)
-          else loop_ rest @@ first :: result
-    in let (bracket, _) = loop_ tokens_ [] in bracket
-  in loop tokens []
-
-
-
-let text = read_line ()
-let tokens = tokenize text
-let () = print_tokens tokens
-let exprs = parse tokens
-let e t = tokenize t |> parse
+let e t = parse "fun a -> b"
